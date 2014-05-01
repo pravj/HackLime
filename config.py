ca3468cba4fed6d7d51d889c5d5381b4223b3849
Pravendra Singh <hackpravj@gmail.com>
2014-05-01 13:29:42 +0530

    * removes MAC integration as I can't test installtion for MAC :D * removes output command * working for error handling

---
+++ config.py
@@ -0,0 +1,24 @@
+"""
+Configurations related to HackLime Package
+
+Author  : Pravendra Singh (pravj)
+Contact : hackpravj@gmail.com (https://pravj.github.io)
+"""
+
+
+# HackerEarth API endpoints
+compile_point = 'http://api.hackerearth.com/code/compile/'
+run_point = 'http://api.hackerearth.com/code/run/'
+
+# your client secret key, needed to work with API
+# you can get one, here : http://www.hackerearth.com/api/register/
+client_secret = 'xxxxxxxxxxxxxxxxxxxx'
+
+# time-limit(in seconds) : max time for which program is allowed to run
+time_limit = 5
+
+# memory-limit(in MB) : max memory allowed to be used by the program
+memory_limit = 262144
+
+# the way in API works, by Default its synchronous
+async = 0
